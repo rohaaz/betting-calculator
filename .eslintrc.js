5aab8130e58dfc6951f6abce90d47ab818189e15
rohaaz <roydenjarvis1996@gmail.com>
2025-04-18 22:21:24 +1000

    y

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,7 @@
-javascript// .eslintrc.js
-module.exports = {
-  extends: 'next/core-web-vitals',
-  rules: {
-    '@typescript-eslint/no-unused-vars': 'off',
-    'react-hooks/exhaustive-deps': 'off'
+{
+  "extends": "next/core-web-vitals",
+  "rules": {
+    "@typescript-eslint/no-unused-vars": "off",
+    "react-hooks/exhaustive-deps": "off"
   }
 }
