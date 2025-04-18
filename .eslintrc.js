a215e052601b8cc3a2dc75d3582cdfbcc75e6176
rohaaz <roydenjarvis1996@gmail.com>
2025-04-18 21:41:53 +1000

    comnit

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+javascript// .eslintrc.js
+module.exports = {
+  extends: 'next/core-web-vitals',
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react-hooks/exhaustive-deps': 'off'
+  }
+}
